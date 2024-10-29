940fa5e7285b8d455fc4d7001a47e65776a3146d
Maverick <levimfranklin123456@gmail.com>
2024-10-28 18:43:27 -0700

    FINALLY GOT FIRST PART OF THE DIAGRAM DONE

--- Lab-04-starter/firmware/src/asmFunc.s
+++ Lab-04-starter/firmware/src/asmFunc.s
@@ -71,48 +71,107 @@ asmFunc:
  
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-   ldr r1, =transaction /* Loads the address of transaction */
-   str r0, [r1] /* Loads the value of transaction into r0 */
     
-   /* Compares transaction against 1000 */
-   cmp r1, #1000
-   bgt transaction_out_of_bounds /* Branch if greater than 1000 */
+   /* Set output variables to 0 */
    
-   /* Compares transaction against -1000 */
-   cmp r1, #-1000
-   blt transaction_out_of_bounds /* Branch if less than 1000 */
+   ldr r1, =we_have_a_problem
+   mov r2, #0
+   str r2, [r1]
    
-   /* If transaction is within bounds */
-   transaction_ok:
-   ldr r1, =balance
-   ldr r2, [r1]
+   ldr r1, =eat_out
+   mov r2, #0
+   str r2, [r1]
    
-   adds r3, r2, r0
+   ldr r1, =stay_in
+   mov r2, #0
+   str r2, [r1]
    
-   bvs overflow_detected
-   ldr r1, =tmpBalance
-   str r3, [r1]
+   ldr r1, =eat_ice_cream
+   mov r2, #0
+   str r2, [r1]
+   
+   ldr r1, =transaction
+   mov r2, #0
+   str r2, [r1]
+   
+   /* Copy r0 to transaction */
+   
+   ldr r1, =transaction
+   str r0, [r1]
+   
+   /* compare transaction to 1000 and -1000 */
+   
+   ldr r5, =0x03E8	/* loads 1000 into register 5 */
+   cmp r0, r5	    /* compares transaction to 1000 */
+   bgt problem	    /* branches to problem if greater than 1000 */
+   
+   ldr r5, =0xFFFFFC18	/* loads -1000 into register 5 (32-bit two's complement) */
+   cmp r0, r5	    /* compares transaction to -1000 */
+   blt problem	    /* branches to problem if less than -1000 */
+   
+   ldr r2, =balance	/* load balance into register 2 */
+   ldr r3, [r2]		/* loads memory address balance in register 2 into register 3 */
+   adds r5, r3, r0	/* does transaction + balance and stores it in register 5 while setting the flags */
+   bvs problem	    /* branches to problem if overflow flag is set */
+   
+   /* continue normal execution here if no problem or overflow */
+   
+   /* balance = tmpBalance */
+   str r5, [r2]
    
    b done
    
-   /* Overflow */
-   overflow:
-    ldr r1, =we_have_a_problem
-    move r0, #1
-    str r0, [r1]
+   /* transaction out of bounds or overflow */
    
+   problem:
+    ldr r3, =we_have_a_problem	    /* loads we_have_a_problem into register 3 */
+    mov r4, #1	    /* sets register 4 to 1 */
+    str r4, [r3]	/* stores value in register 4 into destination address in register 3 */
     
-   /* If transaction is greater than 1000 or less than -1000 */
-   transaction_out_of_bounds:
-    mov r0, #0
-    str r0, [r1]
+    ldr r1, =transaction	/* loads transaction in register 1 */
+    mov r2, #0	    /* sets register 2 to 0 */
+    str r2, [r1]	/* stores value in register 2 into transaction variable */
     
-    ldr r1, =we_have_a_problem
-    move r0, #1
-    str r-, [r1]
+    ldr r2, =balance	    /* loads the address of balance into r2 */
+    ldr r0, [r2]	/* loads the current value of balance into r0 (I KNOW ITS REDUNDANT BUT IM SLOW AND NEED TO SEE IT) */
     
-    ldr r1, =balance
-    ldr r0, [r1]
+    b done	/* branches to done */
+    
+    /* second part of flow diagram */
+    
+    ldr r2, =balance
+    ldr r0, [r2]
+    
+    cmp r0, #0	    /* compares balance to 0 */
+    bgt set_eat_out	    /* branches to eat_out if balance is greater than 0 */
+    
+    blt set_stay_in	    /* branches to stay_in if balance is less than 0 */
+    
+    b set_eat_ice_cream	/* branches to eat_ice_cream if balance neither greater nor less than 0 */
+    
+    set_eat_out:
+    ldr r1, =eat_out	    /* loads address eat_out into register 1 */
+    mov r2, #1	    /* sets register 2 to 1 */
+    str r2, [r1]	/* stores value in register 2 into eat_out variable  */
+    b return_bal
+    
+    set_stay_in:
+    ldr r1, =stay_in	    /* loads address stay_in into register 1 */
+    mov r2, #1		/* sets register 2 to 1 */
+    str r2, [r1]	/* stores value in register 2 into stay_in variable */
+    b return_bal
+    
+    set_eat_ice_cream:
+    ldr r1, =eat_ice_cream	    /* loads address eat_out into register 1 eat_ice_cream */
+    mov r2, #1		/* sets register 2 to 1 */
+    str r2, [r1]	/* stores value in register 2 into eat_ice_cream variable */
+    b return_bal
+    
+    return_bal:
+    ldr r2, =balance	    /* loads the address of balance into r2 */
+    ldr r0, [r2]	/* loads the current value of balance into r0 (yes... again) */
+    
+    b done
     
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
 
